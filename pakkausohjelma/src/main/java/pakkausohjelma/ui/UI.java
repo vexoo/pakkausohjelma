0ec87689e9cd36ba67efb9f0b249914e9042c639
Jani Toivanen <jntv90@gmail.com>
2020-10-29 06:34:00 +0200

    repo alustus

---
+++ pakkausohjelma/src/main/java/pakkausohjelma/ui/UI.java
@@ -0,0 +1,15 @@
+package pakkausohjelma.ui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class UI extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+    }
+}
