0ba9e95d99abafc70c673d5e37a08355e9bc6b26
Jani Toivanen <jntv90@gmail.com>
2020-10-30 08:27:39 +0200

    varmaan turhia javafx alustuksia

--- pakkausohjelma/src/main/java/pakkausohjelma/Main.java
+++ pakkausohjelma/src/main/java/pakkausohjelma/Main.java
@@ -1,8 +1,10 @@
 package pakkausohjelma;
 
+import pakkausohjelma.ui.UI;
+        
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        UI.main(args);
     }
 }
--- pakkausohjelma/src/main/java/pakkausohjelma/ui/UI.java
+++ pakkausohjelma/src/main/java/pakkausohjelma/ui/UI.java
@@ -2,8 +2,14 @@ package pakkausohjelma.ui;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class UI extends Application {
+
+    private static JFileChooser tiedostoValitsija = new JFileChooser();
+    private static FileNameExtensionFilter filter = new FileNameExtensionFilter(
+            ".JPG & .GIF", "jpg", "gif");
 
     public static void main(String[] args) {
         launch(args);
@@ -11,5 +17,14 @@ public class UI extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        valitseTiedosto();
+    }
+
+    private void valitseTiedosto() {
+        tiedostoValitsija.setFileFilter(filter);
+        int arvo = tiedostoValitsija.showOpenDialog(null);
+        if (arvo == JFileChooser.APPROVE_OPTION) {
+            System.out.println(tiedostoValitsija.getSelectedFile().getName());
+        }
     }
 }
