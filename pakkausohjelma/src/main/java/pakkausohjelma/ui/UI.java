fde583a39f38fafb31d3ade6dec100d3ff4d3322
Jani Toivanen <jntv90@gmail.com>
2020-10-31 08:41:25 +0200

    (ruma) käyttöliittymä aloitettu

--- pakkausohjelma/src/main/java/pakkausohjelma/ui/UI.java
+++ pakkausohjelma/src/main/java/pakkausohjelma/ui/UI.java
@@ -1,15 +1,22 @@
 package pakkausohjelma.ui;
 
+import java.io.File;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import javax.swing.JFileChooser;
-import javax.swing.filechooser.FileNameExtensionFilter;
+import javafx.stage.FileChooser;
 
 public class UI extends Application {
 
-    private static JFileChooser tiedostoValitsija = new JFileChooser();
-    private static FileNameExtensionFilter filter = new FileNameExtensionFilter(
-            ".JPG & .GIF", "jpg", "gif");
+    private static FileChooser tiedostoValitsija = new FileChooser();
+    private File tiedosto;
 
     public static void main(String[] args) {
         launch(args);
@@ -17,14 +24,49 @@ public class UI extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        valitseTiedosto();
+        final Label tiedostoNimi = new Label();
+        final Label tiedostoKoko = new Label();
+        
+        Button tiivistaLZW = new Button("Tiivistä tiedosto");
+        tiivistaLZW.setOnAction(e -> {
+        });
+        tiivistaLZW.setVisible(false);
+
+        Button valitseTiedosto = new Button("Valitse tiivistettävä tiedosto");
+        valitseTiedosto.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                FileChooser.ExtensionFilter filter = new FileChooser.ExtensionFilter(
+                        ".JPG & .GIF", "*.jpg", "*.gif");
+                tiedostoValitsija.getExtensionFilters().add(filter);
+
+                tiedosto = tiedostoValitsija.showOpenDialog(primaryStage);
+                if (tiedosto != null) {
+                    tiedostoNimi.setText(tiedosto.getName());
+                    long kokoTavuina = tiedosto.length();
+                    long kokoKiloTavuina = (kokoTavuina / 1024);
+                    tiedostoKoko.setText(String.format("%,d KB", kokoKiloTavuina));
+                    tiivistaLZW.setVisible(true);
+                }
+            }
+        });
+        
+        VBox tiedostoInfo = new VBox(10);
+        tiedostoInfo.getChildren().add(tiedostoNimi);
+        tiedostoInfo.getChildren().add(tiedostoKoko);
+        tiedostoInfo.setAlignment(Pos.BASELINE_CENTER);
+
+        VBox vBox = new VBox(25);
+        vBox.getChildren().add(valitseTiedosto);
+        vBox.getChildren().add(tiedostoInfo);
+        vBox.setAlignment(Pos.BASELINE_CENTER);
+
+        StackPane root = new StackPane();
+        root.getChildren().add(vBox);
+        root.getChildren().add(tiivistaLZW);
+        primaryStage.setScene(new Scene(root, 400, 350));
+        primaryStage.show();
     }
 
-    private void valitseTiedosto() {
-        tiedostoValitsija.setFileFilter(filter);
-        int arvo = tiedostoValitsija.showOpenDialog(null);
-        if (arvo == JFileChooser.APPROVE_OPTION) {
-            System.out.println(tiedostoValitsija.getSelectedFile().getName());
-        }
-    }
+
 }
